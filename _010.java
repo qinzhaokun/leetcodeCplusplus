dc7f1a93894a55d6f891a5c69b8133da38ebcdbf
Zhaokun Qin <qinzhaokun@qq.com>
2016-08-14 22:25:17 +0800

    Create _010.java

---
+++ _010.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(); int n = p.length();
+        boolean [][] dp = new boolean[m+1][n+1];
+        
+        dp[0][0] = true;
+        //s is 
+        for(int i = 1;i <= n;i++){
+            if(p.charAt(i-1) == '*'){
+                dp[0][i] = dp[0][i-1] || (i > 1 && dp[0][i-2]);
+            }
+        }
+        
+        for(int i = 1;i <= m;i++){
+            for(int j = 1;j <= n;j++){
+                if(p.charAt(j-1) == '.'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p.charAt(j-1) == '*'){
+                    //* represents zero characters or * represents more than one characters
+                    if(j >= 2)
+                    // p[j-2] represents zero characters
+                        dp[i][j] = dp[i][j-2];
+                    //p[j-2] represents represents more than one characters
+                    if(j-2 >= 0 && (p.charAt(j-2) == '.') || (p.charAt(j-2) == s.charAt(i-1))){
+                        if(!dp[i][j]){
+                            dp[i][j] = dp[i-1][j];
+                        }
+                    }
+                }
+                else{
+                    dp[i][j] = dp[i-1][j-1] && p.charAt(j-1) == s.charAt(i-1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
