b54506df0815acfd7b5e70f0c7b05569ed7e0385
Zhaokun Qin <qinzhaokun@qq.com>
2016-09-14 10:45:12 +0800

    Update _099.cpp

--- _099.cpp
+++ _099.cpp
@@ -49,3 +49,71 @@ public:
     
     
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* node1;
+    TreeNode* node2;
+    void recoverTree(TreeNode* root) {
+        node1 = NULL;node2 = NULL;
+        TreeNode* last = NULL;
+        while(root){
+            if(root->left){
+                TreeNode* tmp = root->left;
+                while(tmp->right != NULL && tmp->right != root){
+                    tmp = tmp->right;
+                }
+                if(tmp->right ==NULL) {
+                    tmp->right = root;
+                    root = root->left;
+                }
+                else{
+                    tmp->right = NULL;
+                    if(last){
+                        if(last->val > root->val){
+                            if(!node1){
+                                node1 = last;node2 = root;
+                            }
+                            else{
+                                node2 = root;
+                            }
+                            
+                        }
+                    }
+                    last = root;
+                    root = root->right;
+                }
+            }
+            else{
+                if(last){
+                        if(last->val > root->val){
+                            if(!node1){
+                                node1 = last;node2 = root;
+                            }
+                            else{
+                                node2 = root;
+                            }
+                            
+                        }
+                }
+                last = root;
+                root = root->right;
+            }
+        }
+        
+        if(node1){
+            int tmp = node1->val;
+            node1->val = node2->val;
+            node2->val = tmp;
+        }
+    }
+};
